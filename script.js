29f9f79e3e0f60c6b4f8bc8bc374086269109d99
mohitrajputt <85515035+mohitrajputt@users.noreply.github.com>
2024-02-10 23:51:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function playlist() {
+  window.location.href = "playlist.html";
+}
+
+function playmusic() {
+  document.getElementsByClassName("music-player")[0].style.display = "block";
+  let play = document.getElementsByTagName("audio")[0];
+  play.autoplay = true;
+  play.load();
+}
+
+// auto image slider
+
+const slides = document.querySelectorAll(".trending-music .slide-show");
+const intervalTime = 3000; // Time in milliseconds between slides
+let slideIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+setInterval(nextSlide, intervalTime); // Auto slide every 3 seconds
+
+// Initial display
+showSlide(slideIndex);
